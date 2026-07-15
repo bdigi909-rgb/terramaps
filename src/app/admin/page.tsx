c686e417e3b15e349d764fe485f062bded36b5b8
BL Digitale <bourigmustapha71@gmail.com>
2026-07-15 15:03:00 +0100

    fix duplicate MessagesAdmin import

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,6 @@
 ﻿"use client";
 import MessagesAdmin from "@/components/MessagesAdmin";
 import RatingsOverview from "@/components/RatingsOverview";
-import MessagesAdmin from "@/components/MessagesAdmin";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
